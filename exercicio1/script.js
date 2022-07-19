c9058372e2fbfbd21e144a4a49f52c8534a3d568
Eduardo Boucault <boucault.labenu@outlook.com>
2022-07-18 22:50:14 -0300

    Tarefa para entrega aula 007, exercício 1

---
+++ exercicio1/script.js
@@ -0,0 +1,32 @@
+//* A)
+
+// let nome;
+
+//* B)
+
+// let idade;
+
+//* C)
+
+// console.log(typeof nome);
+// console.log(typeof idade);
+
+//* D)
+
+//! O console retornou undefined para ambas as variáveis, isso se deve ao fato de nenhum valor ter sido atribuido à elas.
+
+//* E)
+
+let nome = prompt("Qual é seu nome?");
+let idade = prompt("Qual é sua idade?");
+
+//* F)
+
+console.log(typeof nome);
+console.log(typeof idade);
+
+//! O console retornou para ambas as variáveis o tipo string, pois todos as respostas recebidas por um comando prompt retorna uma string.
+
+//* G)
+
+console.log("Olá" , nome , "você tem" , idade , "anos");
