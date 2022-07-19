0fff7858f97c045fab7045f2cf46880002e3b539
Eduardo Boucault <boucault.labenu@outlook.com>
2022-07-19 01:59:48 -0300

    Entrega de tarefa, exercício 1.

--- exercicio1/script.js
+++ exercicio1/script.js
@@ -1,32 +1,34 @@
-//* A)
+//*A)
 
-// let nome;
+// const nome = ""
 
-//* B)
+//*B)
 
-// let idade;
+// let idade
 
-//* C)
+//*C)
 
-// console.log(typeof nome);
-// console.log(typeof idade);
+// console.log(typeof nome , typeof idade)
 
-//* D)
+//*D)
+//O tipo da variável nome apareceu string, devido as aspas como atribuição de tipo porém sem valor indicado.
+//O tipo da variável idade apareceu undefined, foi não foi atribuido nenhum valor à ela.
 
-//! O console retornou undefined para ambas as variáveis, isso se deve ao fato de nenhum valor ter sido atribuido à elas.
+//*E)
 
-//* E)
+let pergunta1 = prompt('Qual é o seu nome?')
+let pergunta2 = prompt('Qual é a sua idade?')
 
-let nome = prompt("Qual é seu nome?");
-let idade = prompt("Qual é sua idade?");
+const nome = pergunta1
+let idade = pergunta2
 
-//* F)
+//*F)
 
-console.log(typeof nome);
-console.log(typeof idade);
+console.log(typeof nome , typeof idade)
+//Os tipos das variáveis mudaram devido ao fato de serem atribuidos novas variáveis a elas com tipos diferentes.
+//Antes a const nome = "" era uma string e recebeu um prompt como novo valor, então ela se manteve string
+//E a variável let idade recebeu um novo valor de prompt que sempre recebe um string, ou seja, mudou seu valor original de undefined para string.
 
-//! O console retornou para ambas as variáveis o tipo string, pois todos as respostas recebidas por um comando prompt retorna uma string.
-
-//* G)
-
-console.log("Olá" , nome , "você tem" , idade , "anos");
+//*G)
+let bemVindo = alert ("Olá " + nome + " você tem " + idade + " anos.")
+console.log("Olá " , nome , "você tem " , idade , "anos.")
